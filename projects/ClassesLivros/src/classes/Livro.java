fcaf6e2c9f2cf21b115f7e986a4e3b66bf55ea17
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2023-04-13 19:43:11 -0300

    Subindo os projetos já desenvolvidos

---
+++ projects/ClassesLivros/src/classes/Livro.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author felipe.ssantos80
+ */
+public class Livro {
+    String nome, autor, editora, tipoCapa;
+    int paginas;
+}
---
+++ projects/ClassesPessoas/src/main/Lampada.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author felipe.ssantos80
+ */
+public class Lampada {
+    String modelo, marca, tipo, casquilho, cor, formato;
+    int voltagem, temperatura;
+    double watts;
+}
